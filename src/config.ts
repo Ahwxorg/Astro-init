b836f0e8232f24c82b7d276e1c422024904e386d
Ahwx <ahwx@ahwx.org>
2024-01-04 01:21:37 +0100

    feat(config.ts): adds `globals`

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site.
+
+export const SITE_TITLE = "Ahwxorg -> Astro-init"
+export const SITE_DESCRIPTION = "A cozy Astro template"
+export const HOMEPAGE_URL = "https://example.org"
